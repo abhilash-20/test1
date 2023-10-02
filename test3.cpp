7ae5cc3e244743c8ab112fe521f2d623f8d68510
Abhilash <abhilashkashyap33@gmail.com>
2023-10-02 20:15:55 +0530

    vector using cpp

--- test3.cpp
+++ test3.cpp
@@ -1,7 +1,27 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 int main()
 {
-    
+    vector <int> v;
+    int s,ele;
+    cout<<"enter the number of elements you want to enter"<<endl;
+    cin>>s;
+    cout<<"enter the elements:"<<endl;
+    for(int i=0;i<s;i++)
+    {
+        cin>>ele;
+        v.push_back(ele);
+    }
+    cout<<"the vector is: (";
+    for(int i=0;i<s;i++)
+    {
+        cout<<v.at(i);
+        if(i!=(s-1))
+        {
+            cout<<",";
+        }
+    }
+    cout<<")";
     return 0;
 }
